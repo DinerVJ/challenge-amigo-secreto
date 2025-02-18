a053f477bf1220cbe6cf60eba8b3a2588b510808
Diner VJ <dinervj18@gmail.com>
2025-02-18 15:42:07 -0500

    Se desarrollo las funciones para el reto

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let namesArray = [];
+let lsNames = document.getElementById('listaAmigos');
+let r = document.getElementById('resultado');
+
+function agregarAmigo(){
+    let inputName = document.getElementById('amigo').value;
+    if (inputName === ""){
+        alert("Ingrese un nombre!");
+        return;
+    }
+    if (!/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/.test(inputName)){
+        alert("Digite solo letras");
+        return;
+    }
+
+    namesArray.push(inputName);
+
+    lsNames.innerHTML = namesArray.map(n => `<li>${n}</li>`).join("");
+    cleanInputName();
+}
+function cleanInputName(){
+    let cin = document.getElementById('amigo');
+    cin.value = '';
+}
+function sortearAmigo() {
+    if (namesArray.length === 0) {
+        alert("No hay nombres en la lista")
+        return;
+    }
+    let randomName = Math.floor(Math.random()*namesArray.length);
+    let secretFriend = namesArray[randomName];
+    console.log(secretFriend);
+    r.innerHTML = `El amigo secreto sorteado es: ${secretFriend}`;
+    namesArray=[];
+    lsNames.innerHTML = "";
+}
